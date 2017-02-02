2833be29689eb3bb80f372d2c151aa503f4c1196
Alex Raubach & Erica Forget & James Wilson & Joshua Lucas <pair+antigrapist.erica42.jdubz1982.mithridates01@devbootcamp.com>
2017-02-02 14:38:12 -0600

    add question.rb user_helper.rb question.erb

---
+++ app/controllers/question.rb
@@ -0,0 +1,16 @@
+get '/questions/new' do
+  return erb :'404' unless current_user
+  @id = current_user.id
+erb :'questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
