44b496d726063a3c5fb4f0b3e1bfc53281a2c472
Erica Forget <erica.forget@gmail.com>
2017-02-06 09:46:05 -0600

    comment on answer works

--- public/js/application.js
+++ public/js/application.js
@@ -4,26 +4,24 @@ $(document).ready(function() {
     e.preventDefault();
     $(this).hide();
     $(this).next().show();
-  });
+    var id = $(this).attr("id");
+  })
+
   $(".comments").on("submit", ".hidden-form", function(e){
     e.preventDefault();
-    var here = this
+    var here = this;
     var formData = $(this).serialize();
-    // var method   = $(this).attr('method');
-    // var action   = $(this).attr('action');
-    var answer   = $(this).parent().children(":first");
-    // console.log(this.action)
+    var commentId = $(this).attr("id");
+    this.reset();
     $.ajax({
       url: this.action,
       type: this.method,
       data: formData
     })
     .done(function(response){
-      // $(here).parent().children(':last').append(response);
-      console.log(response);
-      console.log($(here).parent().children());
-      // $(here).parent().children(':last').append(response);
+      $("ol." + commentId).append(response);
     })
-    // still working on this.
+    $(".hidden-form").hide();
+    $(".comment-comment").show();
   })
 });
