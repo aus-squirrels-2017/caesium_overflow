7d381a26838d34086a7fa7b18e3640f412c3ed47
Erica Forget <erica.forget@gmail.com>
2017-02-05 19:51:49 -0600

    in the middle of creating comment for comments form

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -4,7 +4,18 @@ end
 
 post '/comments/:id' do
   question = Question.find(params[:id])
-  question.comments << Comment.new(body: (params[:new_post]))
+  question.comments << Comment.new(body: (params[:new_post]), user_id: current_user)
   question.save
   redirect "/questions/#{question.id}"
+end
+
+post '/comments/comments/:id' do
+  comment = Comment.find(params[:id])
+  comment.comments << Comment.new(body: (params[:new_post]), user_id: current_user)
+  comment.save
+  if request.xhr?
+    comment.body.to_s
+  else
+    redirect "/questions/#{comment.id}"
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  // comments on comments:
+  $(".comment-comment").on("click", function(e){
+    e.preventDefault();
+    $(this).hide();
+    $(this).next().show();
+  });
+  $(".comments").on("click", ".hidden-form", function(e){
+    e.preventDefault();
+    console.log($(this).text());
+    var method = $(this).attr('method');
+    var action = $(this).attr('action');
+    var answer = $(this).parent().children(":first");
+    var formData = $(this).serializeArray();
+    // $.ajax({
+      // url: action
+      // type: method
+      // data:
+    // })
 
+  })
 });
